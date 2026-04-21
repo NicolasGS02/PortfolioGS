c860a9725a19c903ba2b21b96e1e7983e1d7abce
NicolasGS02 <nicolasgonsan2004@gmail.com>
2026-04-21 18:16:17 +0200

    Implementación de API para formulario y adaptación para funciinamiento del TOAST

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,17 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const form = document.querySelector("#contact form");
+  const form = document.getElementById("contact-form");
 
-  form.addEventListener("submit", function(e) {
+  form.addEventListener("submit", async function(e) {
     e.preventDefault();
 
-    // Mostrar toast
-    const toastEl = document.getElementById('liveToast');
-    const toast = new bootstrap.Toast(toastEl);
-    toast.show();
+    const data = new FormData(form);
 
-    // Limpiar formulario
-    form.reset();
+    try {
+      const response = await fetch("https://formspree.io/f/xlgapryg", {
+        method: "POST",
+        body: data,
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+
+      if (response.ok) {
+        //Mostrar toast
+        const toastEl = document.getElementById('liveToast');
+        const toast = new bootstrap.Toast(toastEl);
+        toast.show();
+
+        //Limpiar formulario
+        form.reset();
+      } else {
+        alert("Error al enviar el mensaje");
+      }
+
+    } catch (error) {
+      console.error(error);
+      alert("Error de conexión");
+    }
   });
 
 });
